43b923bd53916182d015f0b2ee70908b7711f0ff
yan-kang <2929481692@qq.com>
2021-04-14 23:59:06 +0800

    update

---
+++ src/main/java/com/kang/computer_room_management/common/domain/UsageRecord.java
@@ -0,0 +1,95 @@
+package com.kang.computer_room_management.common.domain;
+
+import java.util.Date;
+
+public class UsageRecord {
+    private Integer id;
+
+    private Integer uid;
+
+    private Integer cid;
+
+    private Integer rid;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Integer status;
+
+    private Double cost;
+
+    private Date reqdate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Date getReqdate() {
+        return reqdate;
+    }
+
+    public void setReqdate(Date reqdate) {
+        this.reqdate = reqdate;
+    }
+}
