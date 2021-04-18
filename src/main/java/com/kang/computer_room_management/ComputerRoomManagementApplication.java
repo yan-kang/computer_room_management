70a95490537a28f1e49e2593ef613642ef1dde15
yan-kang <2929481692@qq.com>
2021-04-18 13:21:26 +0800

    更改注册时前端验证逻辑以及输入实时验证

--- src/main/java/com/kang/computer_room_management/ComputerRoomManagementApplication.java
+++ src/main/java/com/kang/computer_room_management/ComputerRoomManagementApplication.java
@@ -2,12 +2,19 @@ package com.kang.computer_room_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ComputerRoomManagementApplication {
+public class ComputerRoomManagementApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ComputerRoomManagementApplication.class, args);
     }
 
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
